df146d1bb73302b6f098a3bdd0d3c4049d7ffbf3
Adrien Giraud <adriengb@gmail.com>
2021-09-01 12:42:03 +0200

    add chart

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,17 @@
 import base64
 import requests
+import json
 
 import streamlit as st
+import plotly.graph_objects as go
+
 
 headers = {
         "Content-Type": "text/plain"
     }
 
 st.title("Welcome to the demo!")
-url = st.text_input(label="api url")
+url = st.text_input(label="Please paste here the url of the service we provide you")
 instructions = "Either upload your own image or select from the sidebar to get a preconfigured image. " \
                "The image you select or upload will be fed through the algorithm and the output will be displayed to the screen."
 st.write(instructions)
@@ -17,11 +20,18 @@ image_file = st.file_uploader('Upload An Image')
 
 if image_file:
     encoded_string = base64.b64encode(image_file.read())
-    st.title("Here is the image you've selected")
+    st.subheader("Here is the image you've selected")
     st.image(image_file)
     response = requests.request("POST", url, headers=headers, data=encoded_string)
-    st.title("Here is the most likely prediction:")
-    st.write(response.text)
-
+    pred = json.loads(response.text)
+    st.subheader("Here is the most likely prediction")
+    most_likely_cat = max(pred, key=pred.get)
+    confidence = max(pred.values())
+    st.write("{} with a confidence of {}%".format(most_likely_cat, round(confidence*100, 0)))
+    fig = go.Figure(go.Bar(
+        x=list(pred.values()),
+        y=list(pred.keys()),
+        orientation='h'))
+    st.plotly_chart(figure_or_data=fig)
 
 st.write("Made with ♡ by the DS team")
